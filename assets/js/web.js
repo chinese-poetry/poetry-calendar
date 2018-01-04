593722a07979977d704e0dd2df1e0751be119b85
jackeyGao <gaojunqi@outlook.com>
2018-01-04 17:32:25 +0800

    update

--- assets/js/web.js
+++ assets/js/web.js
@@ -17,3 +17,17 @@ function monDay(d) {
         diff = d.getDate() - day + (day == 0 ? -6:1);
     return new Date(d.setDate(diff));
 }
+
+function weekNumber(d) {
+    // Copy date so don't modify original
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    // Set to nearest Thursday: current date + 4 - current day number
+    // Make Sunday's day number 7
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+    // Get first day of year
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    // Calculate full weeks to nearest Thursday
+    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+    // Return array of year and week number
+    return [d.getUTCFullYear(), weekNo];
+}
