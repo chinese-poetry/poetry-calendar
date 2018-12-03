00b760420355f9f477911290515758dbe1170c63
jackeyGao <gaojunqi@outlook.com>
2018-12-03 13:28:10 +0800

    add last next

--- assets/js/web.js
+++ assets/js/web.js
@@ -1,5 +1,30 @@
 window.day = new Date();
 
+
+JIJIEMAP = {
+    chun: 'ying',
+    xia: 'he',
+    qiu: 'ju',
+    dong: 'mei'
+}
+
+
+Date.prototype.Format = function (fmt) { //author: meizz 
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+
 function weekDays(current) {
     var week= new Array(); 
     // Starting Monday not Sunday
@@ -110,7 +135,6 @@ function last() {
 function setContentP (content) {
     if (content instanceof Array) {
         var ps = content
-        console.log(ps)
     } else {
         var ps = content.split('\n');
     }
@@ -129,4 +153,46 @@ function setContentP (content) {
 
 function pad(d) {
     return (d < 10) ? '0' + d.toString() : d.toString();
+}
+
+function datediff(first, second) {
+    // Take the difference between the dates and divide by milliseconds per day.
+    // Round to nearest whole number to deal with DST.
+    return Math.abs(Math.round((second-first)/(1000*60*60*24)));
+}
+
+function getJijie (today) {
+    var year = today.getFullYear();
+    var midYear = new Date(year, 05, 30)
+
+    if (today > midYear) {
+        var cf = new Date(year + 1, 02, calendar.getTerm(2019, 6));
+        var dz = new Date(year, 11, calendar.getTerm(2019, 24));
+    } else {
+        var cf = new Date(year, 02, calendar.getTerm(2019, 6));
+        var dz = new Date(year - 1, 11, calendar.getTerm(2019, 24));
+    }
+
+    var xz = new Date(year, 05, calendar.getTerm(2019, 12));
+    var qf = new Date(year, 08, calendar.getTerm(2019, 18));
+
+    var siJi = {
+        cf: datediff(cf, today), 
+        xz: datediff(xz, today), 
+        qf: datediff(qf, today), 
+        dz: datediff(dz, today)
+    }
+
+    var min = Math.min(siJi.cf, siJi.xz, siJi.qf, siJi.dz);
+
+    if (min === siJi.cf) {
+        return 'chun'
+    } else if (min === siJi.xz) {
+        return 'xia'
+    } else if (min === siJi.qf) {
+        return 'qiu'
+    } else if (min === siJi.dz) {
+        return 'dong'
+    }
+    return 'chun'
 }
